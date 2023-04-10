d781923ecaf146b4a315444b5e2f520bbd2b0f0c
Sam Morton <sammorton44@gmail.com>
2023-04-09 19:51:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Dartmouth College<br />
+        Geisel School of Medicine
+      </h2>
+      <h3>Privacy Policy & Terms of Service. Effective Date: April 9, 2023</h3>
+      <span>
+        Welcome to our data collection initiative. Your participation is essential for our research, and we value your trust in us. We are committed to protecting the privacy and security of your personal information. This Privacy Policy and Terms of Service outline the information we collect, how we use it, and the steps we take to ensure your data remains secure.
+        By participating in our data collection, you consent to the collection and use of your information in accordance with this policy. If you do not agree with the policy, please do not participate in our data collection activities.
+      </span>
+      <h3>1. Information collection and use</h3>
+      <span>
+        We collect personal and demographic information that you voluntarily provide when you participate in our data collection activities. This may include, but is not limited to, your name, age, gender, email address, and other demographic information.
+        We use this information to analyze data, understand trends, and draw conclusions for our research. We may also use your email address to contact you for follow-up purposes or to inform you of the study's results.
+      </span>
+      <h3>2. Data anonymization and confidentiality</h3>
+      <span>
+        We are committed to ensuring that your data remains confidential. We will remove any personally identifiable information from the data before it is analyzed or shared with other researchers. We may publish aggregated, anonymized data from our data collection activities in research papers, presentations, or other forms of communication but will not disclose any information that could identify you.
+      </span>
+      <h3>3. Data retention and security</h3>
+      <span>
+        We will retain your personal information for the duration of the study and in accordance with applicable laws and regulations. We implement appropriate security measures to protect your information from unauthorized access, disclosure, alteration, or destruction.
+      </span>
+      <h3>4. Third-party services</h3>
+      <span>
+        We do not sell, trade, or otherwise transfer your personal information to third parties. However, we may use third-party services, such as online data collection platforms, to collect and store your data. These third-party services have their privacy policies and terms of service, which we encourage you to review.
+      </span>
+      <h3>5. Your rights and choices</h3>
+      <span>
+        You have the right to access, correct, or delete your personal information at any time. If you wish to exercise any of these rights, please contact us at [Contact Email]. Participation in our data collection activities is voluntary, and you may withdraw at any time without any negative consequences.
+      </span>
+      <h3>6. Changes to this privacy policy and terms of service</h3>
+      <span>
+        We may update this Privacy Policy and Terms of Service from time to time. Any changes will be effective upon posting on this web page. We encourage you to review this page periodically to stay informed about our privacy practices.
+      </span>
     </div>
   );
 }
